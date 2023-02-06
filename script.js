c3be0bc89e2908e63b4aff9c43db0920e3144261
Júlia Tibes <julia.tibes@gmail.com>
2023-02-06 14:01:13 -0300

    javascript ajusts

--- script.js
+++ script.js
@@ -1,53 +1,6 @@
-// document.getElementById('submit').onclick = function(){
-//     var value = document.getElementsByName('rating');
-//     for (var radio of value){
-//     if (radio.checked) {    
-//     console.log(radio.value);
-//     }}}
+const path = window.location.href;
+const rate = path.slice(-1);
 
-    
-
-
-
-
-// let radioBtn = document.querySelectorAll('input[name="rating"]');
-// let selectedRadio = document.getElementById('selectedRadio')
-let btnSubmit = document.querySelector('button');
 let result = document.querySelector('span');
-
-
-btnSubmit.addEventListener('click', () => {
-    let selected = document.querySelector('input[type="radio"]:checked');
-    result.innerText = selected.value
-    console.log(selected.value)
-});
-
-// radioBtn.array.forEach(radioBtn => {
-//     radioBtn.addEventListener('focus', findSelected);
-// });
-
-
-// function radioSelected () {
-//     const radio = document.getElementsByClassName('.rating');
-//     let value = radio.input[radio.checked].value;
-//     console.log(value)
-// }
-
-
-// function radioSelected(){
-//     const radio = document.getElementById('#rating');
-//     const value = radio.value;
-//     console.log(value);
-// };
-
-
-// const btn = document.querySelector('#submit');
-
-// btn.addEventListener('click', function(){
-//     const radio = document.querySelector('#rating')
-//     const value = radio.value;
-//     console.log(value)
-// });
-
-
+result.innerText = `You selected out ${rate} of 5`
 
